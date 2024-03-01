62d3dcda507be3d326690a64932ba4175045a4e2
Fuyad Hasan Fahim <fuyad.dev@gmail.com>
2024-03-01 20:29:26 +0600

    Main page created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 export default function App() {
     return (
         <>
+            <SpeedInsights />
             <Navbar />
             <Main />
         </>
