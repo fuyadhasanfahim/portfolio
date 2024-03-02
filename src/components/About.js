bb634515e4a62cd8262b5c371b4b0a0ed5a2c8ed
Fuyad Hasan Fahim <fuyad.dev@gmail.com>
2024-03-02 11:10:19 +0600

    "Some adjustments"

--- src/components/About.js
+++ src/components/About.js
@@ -2,41 +2,44 @@ import React from "react";
 
 export default function About() {
     return (
-        <div className="mt-10 max-w-7xl mx-auto h-auto border-b-2 border-[#f5deb3] pb-16">
-            <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-start">
-                <div className="text-2xl font-medium mb-6 md:mt-0">
-                    <h1>About</h1>
-                </div>
-                <div className="text-4xl">
-                    <p>
-                        A multi-disciplinary freelancer with a background in
-                        graphic design and a sushi enthusiasm. I 've had over
-                        five years of experience and retain a design degree.
-                    </p>
-                    <div>
-                        <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
-                            <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
-                                22{" "}
-                                <span className="text-2xl font-medium text-start absolute ml-2">
-                                    completed projects
-                                </span>
-                            </h1>
-                        </div>
-                        <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
-                            <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
-                                03{" "}
-                                <span className="text-2xl font-medium text-start absolute ml-2">
-                                    years experience
-                                </span>
-                            </h1>
-                        </div>
-                        <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
-                            <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
-                                01{" "}
-                                <span className="text-2xl font-medium text-start absolute ml-2">
-                                    awards for projects
-                                </span>
-                            </h1>
+        <div className="border-b-2 border-[#f5deb3]">
+            <div className="mt-10 max-w-7xl mx-auto h-auto mb-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 justify-between items-start">
+                    <div className="text-2xl font-medium mb-6 md:mt-0">
+                        <h1>About</h1>
+                    </div>
+                    <div className="text-4xl">
+                        <p>
+                            A multi-disciplinary freelancer with a background in
+                            graphic design and a sushi enthusiasm. I 've had
+                            over five years of experience and retain a design
+                            degree.
+                        </p>
+                        <div>
+                            <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
+                                <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
+                                    22{" "}
+                                    <span className="text-2xl font-medium text-start absolute ml-2">
+                                        completed projects
+                                    </span>
+                                </h1>
+                            </div>
+                            <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
+                                <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
+                                    03{" "}
+                                    <span className="text-2xl font-medium text-start absolute ml-2">
+                                        years experience
+                                    </span>
+                                </h1>
+                            </div>
+                            <div className="border-t-2 border-[#f5deb3] mt-10 ml-0 md:ml-20 relative">
+                                <h1 className="mt-4 ml-4 text-7xl font-semibold relative">
+                                    01{" "}
+                                    <span className="text-2xl font-medium text-start absolute ml-2">
+                                        awards for projects
+                                    </span>
+                                </h1>
+                            </div>
                         </div>
                     </div>
                 </div>
